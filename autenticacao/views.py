1227e1436ac861a574566d9685f8263b60c74f05
Ghabryel <ghabryelhenrique@hotmail.com>
2022-07-17 13:49:22 -0300

    PyStackWeek - Aula 01

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,13 +1,18 @@
-from asyncio import constants
-from django.http import HttpResponse
-from django.shortcuts import redirect, render
-from .utils import password_is_valid
+from django.shortcuts import redirect, render, get_object_or_404
+from .utils import password_is_valid, email_html
 from django.contrib.auth.models import User
-from django.contrib import messages
-from django.contrib import auth
+from django.contrib import auth, messages
+from django.contrib.messages import constants
+import os
+from django.conf import settings
+from .models import Ativacao
+from hashlib import sha256
+
 
 def cadastro(request):
     if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect('/')
         return render(request, 'cadastro.html')
     elif request.method == "POST":
         username = request.POST.get('usuario')
@@ -24,22 +29,34 @@ def cadastro(request):
                                             password=senha,
                                             is_active=False)
             user.save()
-            messages.add_messages(request, constants.SUCCESS, 'Usuario cadastrado com sucesso')
-            return redirect('/auth/logar')
-        except:
-            messages.add_messages(request, constants.ERROR, 'Erro interno do servidor')
-            return redirect('/auth/cadastro')
 
+            token = sha256(f"{username}{email}".encode()).hexdigest()
+            ativacao = Ativacao(token=token, user=user)
+
+            ativacao.save()
+
+            path_template = os.path.join(settings.BASE_DIR, 'autenticacao/templates/emails/cadastro_confirmado.html')
+            email_html(path_template, 'Cadastro confirmado', [email, ], username=username, link_ativacao="127.0.0.1:8000/auth/ativar_conta/{token}")
+
+            messages.add_message(request, constants.SUCCESS, 'Usuario cadastrado com sucesso')
+            return redirect('/auth/logar')
+
+        except:
+            messages.add_message(request, constants.ERROR, 'Erro interno do servidor')
+            return redirect('/auth/cadastro')
 
 
 def logar(request):
     if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect('/')
         return render(request, 'login.html')
     elif request.method == "POST":
         username = request.POST.get('usuario')
         senha = request.POST.get('senha')
 
         usuario = auth.authenticate(username=username, password=senha)
+        print(usuario)
 
         if not usuario:
             messages.add_message(request, constants.ERROR, 'Username ou senha inválidos')
@@ -47,3 +64,23 @@ def logar(request):
         else:
             auth.login(request, usuario)
             return redirect('/')
+
+
+def sair(request):
+    auth.logout(request)
+    return redirect('/auth/logar')
+
+
+def ativar_conta(request, token):
+    token = get_object_or_404(Ativacao, token=token)
+    if token.ativo:
+        messages.add_message(request, constants.WARNING, 'Essa token já foi usado')
+        return redirect('/auth/logar')
+
+    user = User.objects.get(username=token.user.username)
+    user.is_active = True
+    user.save()
+    token.ativo = True
+    token.save()
+    messages.add_message(request, constants.SUCCESS, 'Conta ativa com sucesso')
+    return redirect('/auth/logar')
